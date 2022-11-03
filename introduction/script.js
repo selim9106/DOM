7d94ff7b8ec8276ee5dac6c40789fc67a3eee850
Selim Elyahyioui <selim.elyahyioui@gmail.com>
2022-11-03 12:46:41 +0100

    update

--- introduction/script.js
+++ introduction/script.js
@@ -4,26 +4,13 @@
 
 document.title = "Modifying the DOM";
 
-/*------------Change the background color of the body to hot pink (#FF69B4)----------*/
+// todo: Change the background color of the body:
 
-/*document.body.style.backgroundColor = "#FF69B4"*/
-/*document.body.style.backgroundColor = "rgb(255,105,180)";*/
-document.body.style.backgroundColor =
-  "#DFF0FF"; /*bleu pastel moins agressif pr les yx */
+document.body.style.backgroundColor = "#DFF0FF";
 
-/*----------------------Using the children method and a for ... of loop, display every children elements of the second child element of your document (display all children elements of the <body>)-----------------*/
+// todo: Using the children method and a for ... of loop, display all children elements of the <body>:
 
-let bodychildren = document.body.children;
-for (let element of bodychildren) {
-  console.log(element);
-}
-
-/*let bodychildren = document.body.getElementsByTagName();
-let bodychildren = arr [];
-for (let element of bodychildren) {
-  console.log(element);
-}*/
-
-/*const bodychildren = Array.from(document.body.getElementsByTagName())*/
-
-/**/
+// let bodychildren = document.body.children;
+// for (let element of bodychildren) {
+//   console.log(element);
+// }

fe9e64b611a4c01de478501824f05df786ba4f64
Selim Elyahyioui <selim.elyahyioui@gmail.com>
2022-11-03 12:48:14 +0100

    update

--- introduction/script.js
+++ introduction/script.js
@@ -1,6 +1,4 @@
-/*
- */
-/*------------------------Change the document title to Modifying the DOM-----------------*/
+// todo: Change the document title to "Modifying the DOM":
 
 document.title = "Modifying the DOM";
 
@@ -10,7 +8,7 @@ document.body.style.backgroundColor = "#DFF0FF";
 
 // todo: Using the children method and a for ... of loop, display all children elements of the <body>:
 
-// let bodychildren = document.body.children;
-// for (let element of bodychildren) {
-//   console.log(element);
-// }
+let bodychildren = document.body.children;
+for (let element of bodychildren) {
+  console.log(element);
+}
